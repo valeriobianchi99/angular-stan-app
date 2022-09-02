da7cdc7a36bc37df31c5c513d0325d48c68e3c01
v.bianchi <valerio.bianchi@euforlegal.it>
2022-09-02 10:52:45 +0200

    first commit

---
+++ src/app/properties/app.properties.ts
@@ -0,0 +1,5 @@
+export class AppProperties {
+    public static readonly author: 'Valerio Bianchi'
+    public static readonly birthDate: 'August 13, 2021'
+    public static readonly password: string = 'e4p9cc1z2s'
+}
